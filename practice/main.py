c361c0437a16b4bb8dd38e1dd894ba055c119dfe
orange-na <otuy1125abman@gmail.com>
2024-04-01 23:39:36 -0700

    perms

--- practice/main.py
+++ practice/main.py
@@ -1,77 +1,31 @@
+from typing import List, Iterator, Tuple
 
 
-def snake_string(chars: str) ->list[list[str]]:
-    results = [[],[],[]]
-    results_index = {0,1,2}
-    insert_index = 1
-    for i, char in enumerate(chars):
-        if i % 4 == 1:
-            insert_index = 0
-        elif i % 2 == 0:
-            insert_index = 1
-        elif i % 4 == 3:
-            insert_index = 2
-        results[insert_index].append(char)
-        for rest_index in results_index - {insert_index}:
-            results[rest_index].append(" ")
-    return results
+def all_perms_v1(elements: List[int]) -> List[List[int]]:
+    result = []
+    if len(elements) <= 1:
+        return [elements]
+
+    for perm in all_perms_v1(elements[1:]):
+        for i in range(len(elements)):
+                result.append(perm[:i] + elements[0:1] + perm[i:])
+    return result
 
 
+def all_perms_v2(elements: List[int]) -> Iterator[List[int]]:
+    if len(elements) <= 1:
+        yield elements
+    else:
+        for perm in all_perms_v2(elements[1:]):
+            for i in range(len(elements)):
+                yield perm[:i] + elements[0:1] + perm[i:]
 
-# def snake_string2(chars: str, depth: int) -> list[list[str]]:
-#     results = [[] for _ in range(depth)]
-#     results_index = { i for i in range(depth)}
-#     insert_index = int(depth / 2)
-
-#     def pos(i):
-#         return i + 1
-    
-#     def neg(i):
-#         return i - 1
-    
-#     op = neg
-
-#     for char in chars:
-#         results[insert_index].append(char)
-#         for rest_index in results_index - {insert_index}:
-#             results[rest_index].append(' ')
-#         if insert_index <= 0:
-#             op = pos
-#         if insert_index >= depth - 1:
-#             op = neg
-#         insert_index = op(insert_index)
-#     return results
-
-def snake_string2(chars: str, depth: int) -> list[list[str]]:
-    results = [[] for _ in range(depth)]
-    results_index = { i for i in range(depth)}
-    insert_index = int(depth / 2)
-
-    def pos(i):
-        return i + 1
-    
-    def neg(i):
-        return i - 1
-    
-    op = neg
-
-    for char in chars:
-        results[insert_index].append(char)
-        for rest_index in results_index - {insert_index}:
-            results[rest_index].append(' ')
-        if insert_index <= 0:
-                op = pos
-        if insert_index >= depth - 1:
-                op = neg
-        insert_index = op(insert_index)
-    return results
-
-import string
 
 if __name__ == '__main__':
-    alphabet = [s for _ in range(2) for s in string.ascii_lowercase]
-    strings = ''.join(alphabet)
-    for line in snake_string2(strings, 8):
-        print(''.join(line))
-
+    elements = [1, 2, 3]
+    for p in all_perms_v1(elements):
+        print(p)
+    print('')
+    for p in all_perms_v2(elements):
+        print(p)
 
