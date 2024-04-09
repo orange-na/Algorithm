6b77737eed3b7c74758c6b9231ebbb789bdbd6a9
orange-na <otuy1125abman@gmail.com>
2024-04-09 14:57:04 -0700

    DoublyLinkedList

--- practice/main.py
+++ practice/main.py
@@ -1,60 +1,43 @@
 class Node():
-    def __init__(self, data, next_node = None):
+    def __init__(self, data, next_node = None, prev_node = None):
         self.data = data
         self.next = next_node
+        self.prev = prev_node
 
 
-class LinkedList():
+class DoublyLinkedList():
     def __init__(self, head = None):
         self.head = head
     
+
     def append(self, data):
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
             return
         
-        last_node = self.head
-        while last_node.next:
-            last_node = last_node.next
+        current_node = self.head
+        while current_node.next:
+            current_node = current_node.next
 
-        last_node.next = new_node
+        current_node.next = new_node
+        new_node.prev = current_node
     
     def insert(self, data):
         new_node = Node(data)
-        new_node.next = self.head
-        self.head = new_node
-
-    def print(self):
-        current_node = self.head
-        while current_node:
-            print(current_node.data)
-            current_node = current_node.next
-
-    
-    def remove(self, data):
-        current_node = self.head
-        if current_node and current_node.data == data:
-            self.head = current_node.next
+        if self.head is None:
+            self.head = new_node
             return
         
-        previous_node = None
-        while current_node and current_node.data != data:
-            previous_node = current_node
-            current_node = current_node.next
+        new_node.next = self.head
+        self.head.prev = new_node
+        self.head = new_node
         
-        if current_node is None:
-            return 
-        
-        previous_node.next = current_node.next
-
-
 
 if __name__ == '__main__':
-    l = LinkedList()
+    l = DoublyLinkedList()
     l.append(1)
     l.append(2)
     l.append(3)
     l.insert(0)
-    l.remove(2)
-    l.print()
+    print(l.head.next.prev.data)
