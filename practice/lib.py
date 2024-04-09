bdcd1be304b4ae6563d2b89c6533c48d7b2d547e
orange-na <otuy1125abman@gmail.com>
2024-04-09 14:23:56 -0700

    LinkedList

--- practice/lib.py
+++ practice/lib.py
@@ -1,48 +1,81 @@
-# from itertools import permutations
-# from collections import Counter
-# import string
+from itertools import permutations
+from collections import Counter
+import string
+from itertools import permutations
+from collections import defaultdict
 
-# arr = [1, 2, 3, 1, 1, 2]
-# strings = 'abc'
-# N = 10
+arr = [1, 2, 3, 1, 1, 2, 7,2]
+arr.append(55)
+arr.pop()
+print(arr)
 
-# a = permutations(strings)
-# print(list(a))
+arr2 = ['a', 'b', 'c']
+strings = 'abc'
+N = 10
 
-# b = Counter(arr)
-# print(b)
+a = permutations(strings)
+print(list(a))
 
-# print(list(reversed(arr)))
+b = Counter(arr)
+print(b)
+print(max(b, key=b.get))
+ans = defaultdict(int)
+print(ans[5]+1)
+print(ans)
 
-# alphabet = string.ascii_lowercase
-# print(alphabet)
+print(list(reversed(arr)))
 
-# ans = {}
-# ans["w"] = ""
-# ans["w"] += "a"
-# ans["b"] = "b"
-# print(ans)
+alphabet = string.ascii_lowercase
+print(alphabet)
 
-# print(chr(97)) ## chr(97) = 'a'
-# print(ord("a")) ## ord('a') = 97
+ans = {}
+ans["w"] = ""
+ans["w"] += "a"
+ans["b"] = "b"
+print(ans)
+
+print(chr(97)) ## chr(97) = 'a'
+print(chr(65)) ## chr(65) = 'A'
+print(ord("a")) ## ord('a') = 97
+print(ord("A")) ## ord('A') = 65
+
+all_permutations = list(permutations(arr2))
+print(all_permutations)
 
 
-# q = [0 for _ in range(N)]
-# print(q)
+
+q = [0 for _ in range(N)]
+print(q)
+
+pairs = [(1, 10), (1, 5), (1, 1)]
+pairs = sorted(pairs)  # タプルの最初の要素でソートされる
+print(pairs)
+
+pairs = [(-3, 2), (-1, 4), (-5, 1)]
+pairs.sort(key=lambda x: (x[1], x[0]))  # タプルの2番目の要素でソートし、同じ場合は1番目の要素でソート
+print(pairs)  
+
+a = [tuple([0, i+1]) for i in range(3)]
+for i in range(3):
+    a[i] = (a[i][0] + 1, a[i][1])
+
+print(a)
 
 
-N = 4
-A = [12, 34, 56, 78]
-X = 1000
+for i in range(0,10,2):
+    print(i)
 
-sum_A = sum(A)
-B = X//sum_A
-C = X%sum_A
+abcd = 'acbd'
+aaa = list(sorted(abcd))
+print(aaa)
 
-tmp = 0
-for i in range(N):
-  tmp += A[i]
-  if tmp >= C:
-    B = B*N + (i+1)
-    print(B)
-    exit()
+tmp = [[0]] * 5
+tmp[0] = 1
+print(tmp)
+
+aa = {1: 2, 3: 4}
+print(aa.get(3))
+
+text = "Python"
+formatted_text = text.center(10, '*')
+print(formatted_text)
--- practice/main.py
+++ practice/main.py
@@ -1,13 +1,60 @@
-from itertools import permutations
+class Node():
+    def __init__(self, data, next_node = None):
+        self.data = data
+        self.next = next_node
 
-string = 'abc'
-perms = list(permutations(string))
 
-print(perms)
+class LinkedList():
+    def __init__(self, head = None):
+        self.head = head
+    
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
 
-for i in range(len(perms)):
-    perms[i] = ''.join(perms[i])
+        last_node.next = new_node
+    
+    def insert(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
 
-print(perms)
-perms.sort()
-print(perms)
+    def print(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data)
+            current_node = current_node.next
+
+    
+    def remove(self, data):
+        current_node = self.head
+        if current_node and current_node.data == data:
+            self.head = current_node.next
+            return
+        
+        previous_node = None
+        while current_node and current_node.data != data:
+            previous_node = current_node
+            current_node = current_node.next
+        
+        if current_node is None:
+            return 
+        
+        previous_node.next = current_node.next
+
+
+
+if __name__ == '__main__':
+    l = LinkedList()
+    l.append(1)
+    l.append(2)
+    l.append(3)
+    l.insert(0)
+    l.remove(2)
+    l.print()
