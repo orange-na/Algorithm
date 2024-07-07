e14cd6c993180eb3f2f17be549a4390cdd92dc0c
orange-na <otuy1125abman@gmail.com>
2024-07-07 11:58:07 +0900

    🚑

--- practice/main.py
+++ practice/main.py
@@ -1,56 +1,28 @@
-"""
-Input 5, 20
-Output
-[[7], [6, 3], [6, 0, 15], [4, 20, 1, 8], [6, 4, 4, 1, 0]]
-            7
-          6   3
-        6   0   15
-      4   20  1   8
-    6   4   4   1   0
-min path = 12
-"""
-from typing import List, Optional
 import random
-
-
-def generate_triangle_list(depth: int, max_num: int) -> List[List[int]]:
-    return [[random.randint(0, max_num) for _ in range(i)] for i in range(1, depth+1)]
-
-
-def print_triangle(data: List[List[int]]) -> None:
-    max_digit = len(str(max([max(l) for l in data])))
-    width = max_digit + (max_digit % 2) + 2
-    for index, line in enumerate(data):
-        numbers = ''.join([str(i).center(width, ' ') for i in line])
-        print(' ' * int(width/2) * (len(data) - index), numbers)
-
-
-def sum_min_path(triangle: List[List[int]]) -> Optional[int]:
-    tree_sum = triangle[:]
-    j, len_triangle = 1, len(triangle)
-    if not len_triangle:
-        return
-
-    while j < len_triangle:
-        line = triangle[j]
-        line_path_sum = []
-        for i, value in enumerate(line):
-            if i == 0:
-                sum_value = line[i] + tree_sum[j-1][0]
-            elif i == len(line) - 1:
-                sum_value = line[i] + tree_sum[j-1][i-1]
-            else:
-                min_path = min(tree_sum[j-1][i-1], tree_sum[j-1][i])
-                sum_value = line[i] + min_path
-            line_path_sum.append(sum_value)
-        tree_sum[j] = line_path_sum
-        j += 1
-    return min(tree_sum[-1])
+from enigma import ALPHABET, PlugBoard, Rotor, Reflector, EnigmaMachine
 
 
 if __name__ == '__main__':
-    data = generate_triangle_list(5, 9)
-    print(data)
-    print_triangle(data)
-    print('min path =', sum_min_path(data))
 
+    get_random_alphabet = lambda: ''.join(
+        random.sample(ALPHABET, len(ALPHABET)))
+    p = PlugBoard(get_random_alphabet())
+    r1 = Rotor(get_random_alphabet(), 3)
+    r2 = Rotor(get_random_alphabet(), 2)
+    r3 = Rotor(get_random_alphabet(), 1)
+
+    r = list(ALPHABET)
+    indexes = [i for i in range(len(ALPHABET))]
+    for _ in range(int(len(indexes)/2)):
+        x = indexes.pop(random.randint(0, len(indexes)-1))
+        y = indexes.pop(random.randint(0, len(indexes)-1))
+        r[x], r[y] = r[y], r[x]
+    reflector = Reflector(''.join(r))
+
+    machine = EnigmaMachine(
+        p, [r1, r2, r3], reflector)
+    s = 'ATTACK SILICON VALLEY'
+    e = machine.encrypt(s)
+    print(e)
+    d = machine.decrypt(e)
+    print(d)
